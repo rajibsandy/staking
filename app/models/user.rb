78b0b3f8fd2dd04fc491041e069d408f306359ce
Prathamesh Wadile <mr.wadileprathamesh@sharkweb.in>
2021-11-20 17:28:32 +0530

    Added password digest to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { minimum: 3, maximum: 25}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 255}
+    has_secure_password
 end
